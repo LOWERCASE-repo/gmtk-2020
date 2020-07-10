74b8ee93b1fdddda0f630ad3f1e6fbd675316e95
LOWERCASE <52083329+LOWERCASE-repo@users.noreply.github.com>
2020-07-10 13:05:27 -0400

    organization

---
+++ Logistics/Extensions.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+internal static class Extensions {
+	
+	internal static Quaternion Rot(this float angle) {
+		return Quaternion.AngleAxis(angle, Vector3.forward);
+	}
+	
+	internal static Vector2 Predict(this Vector2 relPos, Vector2 relVel, float speed) {
+		float a = speed * speed - relVel.sqrMagnitude;
+		float b = Vector2.Dot(relPos, relVel);
+		float det = b * b + a * relPos.sqrMagnitude;
+		if (det < 0f) return relPos;
+		det = Mathf.Sqrt(det);
+		float timeA = b - det;
+		float timeB = b + det;
+		if (timeA > 0f) return relPos + timeA * relVel / a;
+		if (timeB > 0f) return relPos + timeB * relVel / a;
+		return relPos;
+	}
+}
