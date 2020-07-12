62b7231e7f4bc1c68d8fb0c61acd953d880a8f82
LOWERCASE <52083329+LOWERCASE-repo@users.noreply.github.com>
2020-07-11 21:19:25 -0400

    hnhstnauhstneh

--- Minigames/Slots/SlotChoice.cs
+++ Minigames/Slots/SlotChoice.cs
@@ -4,8 +4,14 @@ using UnityEngine.UI;
 class SlotChoice : Choice {
 	
 	bool stopped;
+	float startTime;
+	
+	void Start() {
+		startTime = Time.time;
+	}
 	
 	void Update() {
+		if (Time.time - startTime < 1.5f) return;
 		if (!Input.GetKeyDown(KeyCode.Mouse0)) return;
 		stopped = true;
 		if (transform.localPosition.y < 151f && transform.localPosition.y > 50f) {
@@ -16,7 +22,7 @@ class SlotChoice : Choice {
 	void FixedUpdate() {
 		if (!stopped) {
 			// pos = new Vector2(transform.position.x, transform.position.y - 2);
-			transform.localPosition -= new Vector3(0f, 15f, 0f);
+			transform.localPosition -= new Vector3(0f, 12.5f, 0f);
 			if (transform.localPosition.y <= -200f) {
 				transform.localPosition = new Vector3(0f, 400f, 0f);
 			}
